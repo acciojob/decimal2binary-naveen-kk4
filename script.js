60f9f2955b47179a31c2102c636dc362749add33
naveen-kk4 <133044450+naveen-kk4@users.noreply.github.com>
2023-08-11 02:49:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-function decimalToBinary(num) {
-  //Write you code here
+function decimalToBinary(N) {
+  	let ans = "";
+   while(N>0){
+        if((N & (1)) > 0){
+           ans="1"+ans;
+        }
+        else ans="0"+ans;
+        N>>=1;
+    }
+   return ans;
+	   
   
 }
 
